f0bad0072558a4229f3d3d4b54fc4cb28f1b034d
root <root@freepbx17-dev.celya.fr>
2026-01-06 11:20:21 +0100

    release 6

---
+++ android/app/src/main/kotlin/com/example/voip/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.voip
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterActivity() {
+
+    private var voipEngine: VoipEngine? = null
+    private var methodChannel: VoipMethodChannel? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        val engine = VoipEngine(flutterEngine.dartExecutor.binaryMessenger)
+        voipEngine = engine
+        methodChannel = VoipMethodChannel(this, flutterEngine.dartExecutor.binaryMessenger, engine)
+        engine.initialize(this)
+    }
+
+    override fun onDestroy() {
+        methodChannel?.dispose()
+        voipEngine?.dispose()
+        super.onDestroy()
+    }
+}
