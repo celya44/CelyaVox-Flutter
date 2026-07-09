60d33ef747ba9339a9b1cd29c8768d1c3063891a
root <root@freepbx17-dev.celya.fr>
2026-07-09 18:43:27 +0200

    Divers 3

--- android/app/src/main/cpp/voip_engine.cpp
+++ android/app/src/main/cpp/voip_engine.cpp
@@ -11,6 +11,7 @@
 
 #define LOG_TAG "PjsipNative"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 static JavaVM *g_vm = nullptr;
